ada83e5b333f1d75eb367557865d55244fba8a0c
Your Name <you@example.com>
2026-08-13 14:29:27 +0530

    Fix Railway SSL connection

--- config/db.js
+++ config/db.js
@@ -1,20 +1,5 @@
 const mysql = require("mysql2");
-const fs = require("fs");
 require("dotenv").config();
-
-let sslConfig;
-
-if (process.env.RENDER) {
-    // Render secret file
-    sslConfig = {
-        ca: fs.readFileSync("/etc/secrets/ca.pem")
-    };
-} else {
-    // Local computer
-    sslConfig = {
-        ca: fs.readFileSync("./certs/ca.pem")
-    };
-}
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -23,25 +8,16 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 
-    ssl: sslConfig,
+    ssl: process.env.DB_SSL_CA
+        ? {
+            ca: process.env.DB_SSL_CA,
+            rejectUnauthorized: true
+        }
+        : undefined,
 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-const db = pool.promise();
-
-db.getConnection()
-    .then(connection => {
-        console.log("✅ Database Connected Successfully");
-
-        connection.release();
-    })
-    .catch(error => {
-        console.error("❌ Database Connection Failed");
-        console.error("Code:", error.code);
-        console.error("Message:", error.message);
-    });
-
-module.exports = db;
+module.exports = pool.promise();
